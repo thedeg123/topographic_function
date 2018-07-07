78e98a56f0bb0aae6495a64974460b89aeddbf27
David <dgold@oberlin.edu>
2018-07-06 19:39:46 -0600

    updated command line args, cleaned things up

--- topographic_function.py
+++ topographic_function.py
@@ -10,30 +10,30 @@ def randdec():
     if rand1>rand2:
         return (rand2/rand1)
     return(rand1/rand2)
-import sys
+import argparse
 from sympy import symbols, Symbol
 from sympy.solvers import solve
 from sympy.parsing.sympy_parser import parse_expr,\
 standard_transformations,\
 implicit_multiplication_application
 from sympy.plotting import plot
-
-if len(sys.argv) is 0:
-    print("Please enter a function as an argument")
-    exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("function", help="The function to graph contour lines from")
+parser.add_argument("number_contours", help="The number of contour lines to display", type=int, nargs='?')
+parser.add_argument("step",help="the step of x between contour lines",type=int, default=1, nargs='?')
+args=parser.parse_args()
 #number of contour lines.
-numc=5
-#if len(sys.argv) is 2:
-    #make default step function
-    #if int(sys.argv[2]): sys.argv[2] else: quit()
-func = parse_expr(sys.argv[1].replace('^','**'), transformations=\
+numc = args.number_contours if args.number_contours else 5
+#step between contour lines
+step = args.step if args.step else 1
+func = parse_expr(args.function.replace('^','**'), transformations=\
 (standard_transformations + (implicit_multiplication_application,)))
 y = Symbol('y')
 contour_map = None
 #the number of undefined answers or constant answers to the gived function
 #ex: 0/0 or y=0
 total_lines=0
-for i in range(0,numc, 1):
+for i in range(0,numc, step):
     contours = solve(func-i,y)
     for n,line in enumerate(contours):
         print("Graphing line: y=", line, end='\r');
